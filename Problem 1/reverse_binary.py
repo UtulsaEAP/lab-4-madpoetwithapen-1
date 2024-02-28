5ba213e25b824a8556fe49461cd983e8c2caba8b
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-27 21:19:04 -0600

    problem 1

--- Problem 1/reverse_binary.py
+++ Problem 1/reverse_binary.py
@@ -1,15 +1,17 @@
 """
 Complete the following python code to print the reverse binary representation of positive integer number entered by the user.
 
-Name:
-Lab Time:
+Name: Christine Parrish
+Lab Time: 3:00 PM Friday 
 
 """
 
 
 def reverse_binary():
     user_num = int(input())
-    # YOUR CODE HERE
+    while user_num > 0:
+       print(user_num % 2)
+       user_num = user_num // 2
 
 if __name__ == "__main__":
     reverse_binary()
--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -1,8 +1,8 @@
 """
 Complete the following python code to print the password entered by the user with the modifications described in the readme
 
-Name:
-Lab Time:
+Name: Christine Parrish
+Lab Time: 3:00 PM Friday
 """
 
 def password_mod():
