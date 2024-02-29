d9c937209e4b5d73a04f413b7fecd4300d79256b
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-28 21:30:21 -0600

    reverse strings test

--- Problem 4/reverse_string.py
+++ Problem 4/reverse_string.py
@@ -1,13 +1,28 @@
 """
 Complete the following python code to reverse the string entered by the user.
 
-Name: 
-Lab Time:
+Name: Christine Parrish
+Lab Time: 3:00 PM Friday
 """
 
-def reverse_string():
-    # YOUR CODE HERE
-    
+def reverse_string():   
+    while True:
+        inputString = input()
+        if inputString == "Done":
+            break
+        elif inputString == "done":
+            break
+        elif inputString == "d":
+            break
 
-if __name__ == "__main__":
-    reverse_string()
+        else:
+            i = 0
+            letter = len(inputString)
+            reverseString = ''
+
+            for letter in inputString:
+                reverseString += inputString[(-1 - i)]
+                i += 1
+            print(reverseString)
+        
+reverse_string()
