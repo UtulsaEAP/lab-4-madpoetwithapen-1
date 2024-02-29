cf05c227968d21cb9d81c89e94f48e727caa8a6c
madpoetwithapen <christinekparrish2023@gmail.com>
2024-02-28 20:34:51 -0600

    problem 2

--- Problem 2/password_mod.py
+++ Problem 2/password_mod.py
@@ -8,6 +8,31 @@ Lab Time: 3:00 PM Friday
 def password_mod():
     word = input()
     password = ''
+    letter = len(word)
+    target = 0
+    for letter in word:
+        if word[target] == 'i': 
+            password += '1'
+            target += 1
+        elif word[target] == 'a':
+            password += '@'
+            target += 1
+        elif word[target] == 'm':
+            password += 'M'
+            target += 1
+        elif word[target] == 'B':
+            password += '8'
+            target += 1
+        elif word[target] == 's':
+            password += '$'
+            target += 1
+        else:
+            password += word[target] 
+            target += 1
+
+    password += '!'
+
+    print(password)
 
 
 if __name__ == "__main__":
