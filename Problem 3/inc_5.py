3f40fca3f5f964ee3c632ea50cd83af6f001dc32
madpoetwithapen <christinekparrish2023@gmail.com>
2024-03-19 22:18:53 -0500

    check

--- Problem 3/inc_5.py
+++ Problem 3/inc_5.py
@@ -6,10 +6,20 @@ Lab Time: 3:00 PM Friday
 """
 
 def inc_5(int1,int2):
-    for x in range(int1, int2, 5):
-        print(str(x) + ' ')    
+    num1 = int(input()) 
+    num2 = int(input())
 
+    if num1 <= num2:
+        print(num1, end=" ")
 
+        while num1 + 5 <= num2:
+            num1 += 5
+            print (num1, end=" ")
+
+        print()
+
+    else: 
+        print("Second integer can't be less than the first.")
 
 if __name__ == '__main__':
     inc_5()
